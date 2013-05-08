9b6da9cdad5ecea71e10f35a4df1a8ce925f89e7
MM5 <MaxMorten.Schomann@haw-hamburg.de>
2013-05-08 10:07:55 +0200

    Armer RechnungsTyp

---
+++ src/aip2/m/RechnungsModul/RechnungTyp.java
@@ -0,0 +1,75 @@
+package aip2.m.RechnungsModul;
+
+import java.util.Date;
+
+public final class RechnungTyp {
+
+	private int nr;
+
+	private Date rechnungsDatum;
+
+	private int preis;
+
+	private boolean istBezahlt;
+
+	private int auftragNr;
+
+	RechnungTyp(int nr, Date rechnungsDatum, int preis, boolean istBezahlt,
+			int auftragNr) {
+		this.nr = nr;
+		this.rechnungsDatum = rechnungsDatum;
+		this.preis = preis;
+		this.istBezahlt = istBezahlt;
+		this.auftragNr = auftragNr;
+	}
+
+	public int getNr() {
+		return nr;
+	}
+
+	public Date getRechnungsDatum() {
+		return rechnungsDatum;
+	}
+
+	public int getPreis() {
+		return preis;
+	}
+
+	public boolean isIstBezahlt() {
+		return istBezahlt;
+	}
+
+	public int getAuftragNr() {
+		return auftragNr;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + nr;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RechnungTyp other = (RechnungTyp) obj;
+		if (nr != other.nr)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "RechnungTyp [nr=" + nr + ", rechnungsDatum=" + rechnungsDatum
+				+ ", preis=" + preis + ", istBezahlt=" + istBezahlt
+				+ ", auftragNr=" + auftragNr + "]";
+	}
+
+}
