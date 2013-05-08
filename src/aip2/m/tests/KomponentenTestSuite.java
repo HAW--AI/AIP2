3faecba775d3582a91c4f017864014395a72a108
MM5 <MaxMorten.Schomann@haw-hamburg.de>
2013-05-08 14:15:12 +0200

    Final fix

--- src/aip2/m/tests/KomponentenTestSuite.java
+++ src/aip2/m/tests/KomponentenTestSuite.java
@@ -14,32 +14,33 @@ import aip2.m.RechnungsModul.TestRechungsModul;
 @SuiteClasses({ TestKundenModul.class, TestRechungsModul.class,
 		TestAngebotAuftragModul.class, TestLieferungsModul.class })
 public class KomponentenTestSuite {
-
-	// public static void main(String[] args) {
-	// // IPersistenzSessionIntern persistenz =
-	// // Persistenz.getPersistenzSessionIntern();
-	// // ITransaktionIntern transaktion =
-	// // Transaktion.getTransaktion(persistenz);
-	//
-	// // boolean test = transaktion.checkStartMyTransaction();
-	//
-	// // System.out.println(test);
-	//
-	// // Produkt p = new Produkt("test1", 100);
-	// // persistenz.add(p);
-	//
-	// // Kunde k = new Kunde("na", "hier");
-	// // persistenz.add(k);
-	//
-	// System.out.println("Start");
-	// // Result result = JUnitCore.runClasses(KomponentenTestSuite.class);
-	// // for (Failure failure : result.getFailures()) {
-	// // System.out.println(failure.toString());
-	// // }
-	// // System.out.println(result.wasSuccessful());
-	//
-	// // transaktion.commitTransaction();
-	//
-	// System.out.println("Stop");
-	// }
 }
+
+// public static void main(String[] args) {
+// // IPersistenzSessionIntern persistenz =
+// // Persistenz.getPersistenzSessionIntern();
+// // ITransaktionIntern transaktion =
+// // Transaktion.getTransaktion(persistenz);
+//
+// // boolean test = transaktion.checkStartMyTransaction();
+//
+// // System.out.println(test);
+//
+// // Produkt p = new Produkt("test1", 100);
+// // persistenz.add(p);
+//
+// // Kunde k = new Kunde("na", "hier");
+// // persistenz.add(k);
+//
+// System.out.println("Start");
+// // Result result = JUnitCore.runClasses(KomponentenTestSuite.class);
+// // for (Failure failure : result.getFailures()) {
+// // System.out.println(failure.toString());
+// // }
+// // System.out.println(result.wasSuccessful());
+//
+// // transaktion.commitTransaction();
+//
+// System.out.println("Stop");
+// }
+// }
